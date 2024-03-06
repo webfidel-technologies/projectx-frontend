a2a8052daf75e685b24986cc78be4a5bd96a8f44
princewill-dev <eekudep@gmail.com>
2024-03-06 07:32:35 -0500

    theme three

---
+++ head.php
@@ -0,0 +1,40 @@
+<head>
+	<!-- Meta -->
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="keywords" content="">
+	<meta name="author" content="HugeBinary">
+	<meta name="robots" content="">
+	<meta name="description" content="MoonCart Shop & eCommerce HTML Template">
+	<meta property="og:title" content="MoonCart Shop & eCommerce HTML Template">
+	<meta property="og:description" content="MoonCart Shop & eCommerce HTML Template">
+	<meta name="format-detection" content="telephone=no">
+	
+	<!-- FAVICONS ICON -->
+	<link rel="icon" type="image/x-icon" href="images/favicon.png">
+	
+	<!-- PAGE TITLE HERE -->
+	<title>MoonCart Shop & eCommerce HTML Template</title>
+	
+	<!-- MOBILE SPECIFIC -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<!-- STYLESHEETS -->
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap-select/dist/css/bootstrap-select.min.css">
+	<link rel="stylesheet" type="text/css" href="icons/fontawesome/css/all.min.css">
+	<link rel="stylesheet" type="text/css" href="icons/themify/themify-icons.css">
+	<link rel="stylesheet" type="text/css" href="icons/flaticon/flaticon_mooncart.css">
+	<link rel="stylesheet" type="text/css" href="vendor/swiper/swiper-bundle.min.css">
+	<link rel="stylesheet" type="text/css" href="vendor/nouislider/nouislider.min.css">
+	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+	<link rel="stylesheet" type="text/css" href="vendor/lightgallery/dist/css/lightgallery.css" >
+    <link rel="stylesheet" type="text/css" href="vendor/lightgallery/dist/css/lg-thumbnail.css">
+    <link rel="stylesheet" type="text/css" href="vendor/lightgallery/dist/css/lg-zoom.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	
+	<!-- GOOGLE FONTS-->
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+
+</head>
